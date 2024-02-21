2ff23f6cd7966cf1a7e32f847c198b12b7a9fa92
samidsaputra <160684678+samidsaputra@users.noreply.github.com>
2024-02-21 09:27:54 +0700

    Titik.java

--- Praktikum 1/Titik.java
+++ Praktikum 1/Titik.java
@@ -1,3 +1,10 @@
+/**
+ * File      		: Titik.java		21/02/24
+ * Nama penulis   	: M. Dimas Saputra
+ * NIM               	: 24060122140159
+ * Deskripsi 		: Kelas yang berisi body  MTitik.java
+ * 
+ */
 class Titik
 {
 	double absis;
